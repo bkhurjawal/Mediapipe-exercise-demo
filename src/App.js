30d5733996257109d0359bba30f43ff910d77d7c
Bhavya <bkhurjawal@gmail.com>
2025-05-01 17:00:44 +0530

    Dir fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import './App.css';
-// import 'primereact/resources/themes/saga-blue/theme.css'; // theme
+import './styles/App.css';
 import 'primeflex/primeflex.css';
 import 'primeflex/themes/primeone-light.css';
-import 'primereact/resources/primereact.min.css'; // core css
+import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/saga-green/theme.css';
 
 import { PrimeReactProvider } from 'primereact/api';
-
-import MediaPipeColorChanger from './MediaPipeColorChanger';
+import PoseTrackerWithUpload from './screens/PoseTrackerWithUpload';
 
 function App() {
   return (
     <div className="App">
       <PrimeReactProvider>
-        <MediaPipeColorChanger />
-        {/* <PoseFrame /> */}
-        {/* <PoseTracker/> */}
-        {/* <PoseTrackers /> */}
-        {/* <PoseEstimation /> */}
-        {/* <PoseTrackerWithUpload /> */}
-        {/* <PoseReview /> */}
-        {/* <PoseTrackerWithHolistic /> */}
+        <PoseTrackerWithUpload />
       </PrimeReactProvider>
     </div>
   );
--- src/Messages.js
+++
@@ -1,5 +0,0 @@
-export const Messages = {
-  GET_BACK:
-    'Tilt your camera or go backwards until you are fully inside the red frame.',
-  GREAT_SPOT: "Great! That's the spot!",
-};
